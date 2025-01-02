9b52d669abc55cf004bb587d6ad99eb0be31c73a
bailangvvkg <bailangvvking@outlook.com>
2025-01-02 19:09:00 +0800

    Update Settings.sh
    调整编译器优化程度O3

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -55,7 +55,7 @@ fi
 #编译器优化
 if [[ $WRT_TARGET != *"X86"* ]]; then
 	echo "CONFIG_TARGET_OPTIONS=y" >> ./.config
-	echo "CONFIG_TARGET_OPTIMIZATION=\"-O2 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
+	echo "CONFIG_TARGET_OPTIMIZATION=\"-O3 -pipe -march=armv8-a+crypto+crc -mcpu=cortex-a53+crypto+crc -mtune=cortex-a53\"" >> ./.config
 fi
 
 #修改jdc re-ss-01 (亚瑟) 的内核大小为12M
